6e5e1931b4d3fe528b32b0082afaa2cd9a4a0305
Ankit-0009 <ankitcode2511@gmail.com>
2025-01-30 16:06:56 +0530

    Update CORS configuration to allow requests from specific origin

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(                //this allows us to make requests from the frontend to the backend
     {
         // origin: 'http://localhost:5173/',  //https://damac.onrender.com
-        origin: "*",
+        origin: "https://nimiproject-8sad.onrender.com/",
         credentials: true,
         // methods: ["GET", "POST", "PUT", "DELETE"]
     }
