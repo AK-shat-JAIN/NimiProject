e7f242c4cbc6cfb46e6fcfdd79d60c2901634919
Ankit-0009 <ankitcode2511@gmail.com>
2025-01-30 16:23:49 +0530

    Fix axios base URL by removing trailing slash

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://nimiproject-1.onrender.com/api/v1/"
+const BASE_URL = "https://nimiproject-1.onrender.com/api/v1"
 
 const axiosInstance = axios.create();
 
